b8cdda07aa0c89b8b3fcc81d31cb676567ac57b3
AmmarSaqib <ammarsaqib123@gmail.com>
2021-03-15 14:42:35 +0500

    ports hardcoded

--- backend/app_a/asgi.py
+++ backend/app_a/asgi.py
@@ -6,8 +6,10 @@ Main file to trigger the Uvicorn server
 
 import os
 import uvicorn
+import logging
 from app.main import app
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=os.environ.get("APP_A_PORT"))
+    logging.info(os.environ.get("APP_A_PORT"))
+    uvicorn.run(app, host='0.0.0.0', port=3030)
